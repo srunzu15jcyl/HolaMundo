ad83ec74e5be62a826b95d69e7f24f3913337e43
Marcos Unzueta <srunzu15@usal.es>
2022-02-03 10:22:35 +0100

    Modificación en los textos a imprimir del main.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HOLA MUNDO");
-		System.out.println("HOLA MUNDO");
+		System.out.println("HOLA MUNDO 1");
+		System.out.println("HOLA MUNDO 2");
 
 	}
 
